bbe29545b080b5823fc7150932320183032c6387
sifakzaki <77038328+sifakzaki@users.noreply.github.com>
2023-06-15 23:42:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,10 @@ def predict(file):
     
     return label[result]
 
+@app.get("/")
+def hello_world():
+    return ("hello world")
+    
 @app.post("/predict")
 def classify(input: UploadFile = File(...)):
     print(input.filename)
